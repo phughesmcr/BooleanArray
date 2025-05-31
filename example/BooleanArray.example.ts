e232d30ad4b0e900b9b008fe601a843bd840c5c2
phughesmcr <github@phugh.es>
2025-05-31 17:46:23 +0100

    Fix example file

--- example/BooleanArray.example.ts
+++ example/BooleanArray.example.ts
@@ -1,7 +1,10 @@
+// deno-lint-ignore-file no-console
+/// <reference lib="deno.ns" />
+/// <reference lib="dom" />
+
 /**
+ * @description This module demonstrates the usage of the BooleanArray class.
  * @module
- * @description
- * This module demonstrates the usage of the BooleanArray class.
  */
 
 import { BooleanArray } from "../mod.ts";
@@ -17,60 +20,64 @@ console.log("Size of the array:", array.size);
 
 // set the first bit to true
 console.log("Setting the first bit to true");
-array.setBool(0, true);
+array.set(0, true);
 
 // get the first bit
 console.log("Getting the first bit");
-console.log(array.getBool(0));
+console.log(array.get(0));
 
 // set the first 10 bits to true
 console.log("Setting the first 10 bits to true");
-array.setRange(0, 10, true);
+array.set(0, 10, true);
 
 // get the first 10 bits
 console.log("Getting the first 20 bits");
-console.log(array.getBools(0, 20));
+console.log(array.get(0, 20));
 
 // get the population count
 console.log("Getting the population count (should be 10)");
-console.log(array.getPopulationCount());
+console.log(array.getTruthyCount());
 
 // setting the 33rd bit to true
 console.log("Setting the 33rd bit to true");
-array.setBool(32, true);
+array.set(32, true);
 
 // getting the 33rd bit
 console.log("Getting the 33rd bit");
-console.log(array.getBool(32));
+console.log(array.get(32));
 
 // setting the 34th bit to true
 console.log("Setting the 34th bit to true");
-array.setBool(33, true);
+array.set(33, true);
 
 // getting the 34th bit
 console.log("Getting the 34th bit");
-console.log(array.getBool(33));
+console.log(array.get(33));
 
 // get the population count
 console.log("Getting the population count (should be 12)");
-console.log(array.getPopulationCount());
+console.log(array.getTruthyCount());
 
 // unset the 1st bit
 console.log("Unsetting the 1st bit");
-array.setBool(0, false);
+array.set(0, false);
 
 // get the population count
 console.log("Getting the population count (should be 11)");
-console.log(array.getPopulationCount());
+console.log(array.getTruthyCount());
 
 // get first set bit
 console.log("Getting the first set bit (should be 1 - the 2nd bit)");
-console.log(array.getFirstSetIndex());
+console.log(array.indexOf(true));
 
 // get last set bit
 console.log("Getting the last set bit (should be 33 - the 34th bit)");
-console.log(array.getLastSetIndex());
+console.log(array.lastIndexOf(true));
 
 // get all truthy indices
 console.log("Getting all truthy indices");
 console.log([...array.truthyIndices()]);
+
+// get all values
+console.log("Getting all values");
+console.log([...array]);
